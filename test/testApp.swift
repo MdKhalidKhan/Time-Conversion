8f4ee636fdacdb0318c228286c12ac9f4768217a
Raj <rajkhulna@outlook.com>
2022-08-03 12:21:22 +0600

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Khalid Khan on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
